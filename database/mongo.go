1cac895c37fc467292e8771bea61353650d95407
Pinya Aungkanarungrut <88120284+DEARDOYY@users.noreply.github.com>
2025-01-20 20:32:54 +0700

    connect to mongoDB

--- database/mongo.go
+++ database/mongo.go
@@ -1,1 +1,42 @@
 package database
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"time"
+
+	"go.mongodb.org/mongo-driver/v2/mongo"
+	"go.mongodb.org/mongo-driver/v2/mongo/options"
+)
+
+var (
+	Client *mongo.Client
+	Ctx    = context.TODO()
+)
+
+// ConnectDB เชื่อมต่อ MongoDB และคืนค่า client
+func ConnectDB() *mongo.Client {
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	clientOptions := options.Client().ApplyURI("mongodb://localhost:27017") // เปลี่ยน URI ตามต้องการ
+	client, err := mongo.Connect(clientOptions)
+	if err != nil {
+		log.Fatalf("Error connecting to MongoDB: %v", err)
+	}
+
+	// ทดสอบการเชื่อมต่อ
+	if err := client.Ping(ctx, nil); err != nil {
+		log.Fatalf("MongoDB ping failed: %v", err)
+	}
+
+	fmt.Println("Connected to MongoDB!")
+	Client = client
+	return client
+}
+
+// GetCollection คืนค่าคอลเลกชันตามชื่อ
+func GetCollection(databaseName, collectionName string) *mongo.Collection {
+	return Client.Database(databaseName).Collection(collectionName)
+}
--- main.go
+++ main.go
@@ -1,10 +1,21 @@
 package main
 
-import "go.mongodb.org/mongo-driver/v2/mongo"
+import (
+	"connect-api/database"
+	"log"
+
+	"go.mongodb.org/mongo-driver/v2/mongo"
+)
 
 // MongoDB client
 var client *mongo.Client
 
 func main() {
-
+	// เชื่อมต่อ MongoDB
+	client := database.ConnectDB()
+	defer func() {
+		if err := client.Disconnect(database.Ctx); err != nil {
+			log.Fatalf("Error disconnecting MongoDB: %v", err)
+		}
+	}()
 }
