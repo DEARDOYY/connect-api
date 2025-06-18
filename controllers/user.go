812f30eb43b3f8706910d5cc13c2a48f6529efc3
Pinya Aungkanarungrut <88120284+DEARDOYY@users.noreply.github.com>
2025-06-18 23:58:34 +0700

    update

--- controllers/user.go
+++ controllers/user.go
@@ -30,6 +30,11 @@ func GetUsers(c *gin.Context) {
 		return
 	}
 
+	if len(users) == 0 {
+		c.JSON(http.StatusNotFound, gin.H{"error": "No users found"})
+		return
+	}
+
 	c.JSON(http.StatusOK, users)
 }
 
